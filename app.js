a79a7bdf5a0da580cb5b5bff6714f36de14ca15d
Sonata996 <129296155+Sonata996@users.noreply.github.com>
2024-02-29 20:21:50 +0200

    add Access-Control-Allow

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "React app URL");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
