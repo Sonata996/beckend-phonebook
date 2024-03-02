344ed862a5fc22bdc50985c1ba65eb4965b4c29b
Sonata996 <129296155+Sonata996@users.noreply.github.com>
2024-03-02 21:46:09 +0200

    add middleware isToken

--- router/api/auto-router.js
+++ router/api/auto-router.js
@@ -1,14 +1,15 @@
 import express from "express";
 import authController from "../../controllers/auth-controller.js";
 import isEmpleBody from "../../middlewares/isEmplyBody.js";
+import isToken from "../../middlewares/isToken.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/signup", isEmpleBody, authController.creatUser);
 
-authRouter.post("/login", isEmpleBody, authController.loginUser);
+authRouter.post("/login", isToken, isEmpleBody, authController.loginUser);
 
-authRouter.get("/currant", authController.currentUser);
+authRouter.get("/currant", isToken, authController.currentUser);
 
 authRouter.post("/logout", authController.logoutUser);
 export default authRouter;
