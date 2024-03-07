652e53910049affb9daccfb534695f09844b1bbb
Sonata996 <129296155+Sonata996@users.noreply.github.com>
2024-03-07 23:22:03 +0200

    add routers getContactId, addContact and deleteContact

--- controllers/contact-controller.js
+++ controllers/contact-controller.js
@@ -1,5 +1,6 @@
 import Contact from "../schemas/User.js";
 import HttpError from "../helpers/HttpError.js";
+import User from "../schemas/User.js";
 
 const getAllContacts = async (req, res, next) => {
   const resul = await Contact.find();
@@ -15,7 +16,23 @@ const getbyId = async (req, res, next) => {
   res.json(result);
 };
 
-const add = async (req, res) => {
+const addContacts = async (req, res) => {
   const result = await Contact.create(req.body);
   res.status(201).json(result);
+};
+
+const deletContact = async (req, res) => {
+  const { contactId } = req.params;
+  const result = await User.findOneAndDelete(contactId);
+
+  if (!result) {
+    return next(HttpError(404, "Not found"));
+  }
+  res.json({ message: "contact deleted" });
+};
+
+export default {
+  getAllContacts,
+  getbyId,
+  addContacts,
 };
--- router/api/contact-router.js
+++ router/api/contact-router.js
@@ -1,8 +1,16 @@
 import express from "express";
 import isToken from "../../middlewares/isToken.js";
+import isEmpleBody from "../../middlewares/isEmplyBody.js";
+import contactsControler from "../../controllers/contact-controller.js";
 
 const contactRouter = express.Router();
 
-contactRouter.get("/", isToken);
+contactRouter.get("/", isToken, contactsControler.getAllContacts);
+
+contactRouter.get("/:contactId", isToken, contactsControler.getbyId);
+
+contactRouter.post("/", isToken, isEmpleBody, contactsControler.addContacts);
+
+contactRouter.delete("/", isToken, contactsControler.getAllContacts);
 
 export default contactRouter;
